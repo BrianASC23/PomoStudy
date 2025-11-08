683feccade83d2909d9b8843eb1ef71c85a06847
sadneo <haywardeel@gmail.com>
2025-11-07 20:52:27 -0500

    chore: project setup

---
+++ backend/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "CSE310 dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = 
+      let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python312
+          # python310Packages.pandas
+          # python310Packages.numpy
+          # python312Packages.keyring
+          uv
+        ];
+
+        shellHook = ''
+          exec fish -C "source .venv/bin/activate.fish"
+        '';
+      };
+  };
+}
+
---
+++ backend/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 2025hacks!")
+
+
+if __name__ == "__main__":
+    main()
