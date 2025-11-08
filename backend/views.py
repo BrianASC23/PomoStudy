ad474a3e1d0ced40e8060f79ae9e068a4f46c74c
Daniel Lew <lewdaniel2@gmail.com>
2025-11-08 16:05:02 -0500

    FIX: customizations for voice settings

--- backend/views.py
+++ backend/views.py
@@ -1,25 +1,46 @@
-from flask import Blueprint, request, session, send_file, abort
+from flask import Blueprint, request, session, send_file, abort, jsonify
+import hashlib, os
 
 views = Blueprint("views",__name__)
 
-@views.route('/api/pomodoro-end', methods=["GET"])
-def get_end_sound():
-    from elevenz import end_sound
-    path, file_name = end_sound()
+    
+@views.route('/api/pomdero-start', methods=["POST"])
+def start():
+    data = request.get_json(silent=True)
+
+    voice_id = data.get("voice_id")
+    stability = data.get("stability")
+    similarity_boost = data.get("similarity_boost")
+    style = data.get("style")
+    use_speaker_boost = data.get("use_speaker_boost")
+    
+    personalizations = {
+        "voice_id": data.get("voice_id"),
+        "stability": data.get("stability"),
+        "similarity_boost": data.get("similarity_boost"),
+        "style": data.get("style"),
+        "use_speaker_boost": data.get("use_speaker_boost")
+    }
+
+    from elevenz import start_sound
+    path, file_name = start_sound(personalizations)
     try:
-        return send_file(
-            path,
-            mimetype="audio/mpeg",
-            as_attachment=False,
-            download_name=file_name
+        resp = make_response(
+            send_file(
+                path,
+                mimetype="audio/mpeg",
+                as_attachment=False,
+                download_name=file_name
+            )
         )
     except FileNotFoundError:
         abort(404, description="Audio not found")
+
+@views.route('/api/pomodoro-end', methods=["POST"])
+def end():
+    from elevenz import end_sound
+    path, file_name = end_sound()
     
-@views.route('api/pomdero-start', methods=["GET"])
-def get_start_sound():
-    from elevenz import start_sound
-    path, file_name = start_sound()
     try:
         return send_file(
             path,
